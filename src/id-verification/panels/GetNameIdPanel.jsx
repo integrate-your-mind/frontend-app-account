a2100bfdbbccc9392f538de135700f738cdfb735
Adam Butterworth <abutterworth@edx.org>
2020-04-16 15:30:26 -0400

    Add current account name to GetNameIdPanel

--- src/id-verification/panels/GetNameIdPanel.jsx
+++ src/id-verification/panels/GetNameIdPanel.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useNextPanelSlug } from '../routing-utilities';
 import BasePanel from './BasePanel';
+import { IdVerificationContext } from '../IdVerificationContext';
 
 export default function GetNameIdPanel() {
   const panelSlug = 'get-name-id';
   const nextPanelSlug = useNextPanelSlug(panelSlug);
+  const { nameOnAccount } = useContext(IdVerificationContext);
   return (
     <BasePanel
       name={panelSlug}
       title="GetNameIdPanel"
     >
+      <p>The name on your account is: {nameOnAccount}</p>
       <Link to={nextPanelSlug}>
         <button className="btn btn-primary">Next</button>
       </Link>
