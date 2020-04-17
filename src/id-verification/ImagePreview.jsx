bc23fbb2cc077f9effc2d4564348c636233cfff7
Kyle McCormick <kmccormick@edx.org>
2020-04-17 13:42:30 -0400

    Fix ImagePreview alt text

--- src/id-verification/ImagePreview.jsx
+++ src/id-verification/ImagePreview.jsx
@@ -5,7 +5,7 @@ export default function ImagePreview({ src, alt }) {
   return (
     <div className=" bg-light border border-light p-1 rounded" style={{ maxWidth: '20rem' }}>
       <div className="embed-responsive embed-responsive-4by3">
-        <img className="embed-responsive-item" style={{ objectFit: 'contain' }} src={src} alt={name} />
+        <img className="embed-responsive-item" style={{ objectFit: 'contain' }} src={src} alt={alt} />
       </div>
     </div>
   );
