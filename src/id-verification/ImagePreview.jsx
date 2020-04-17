abae03337b7343fea66401e74c14f43d92196107
Kyle McCormick <kmccormick@edx.org>
2020-04-17 13:56:46 -0400

    Iterating on SummaryPage

--- src/id-verification/ImagePreview.jsx
+++ src/id-verification/ImagePreview.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ImagePreview({ src, alt }) {
+export default function ImagePreview({ src, alt, id }) {
   return (
-    <div className=" bg-light border border-light p-1 rounded" style={{ maxWidth: '20rem' }}>
+    <div id={id} className=" bg-light border border-light p-1 rounded" style={{ maxWidth: '20rem' }}>
       <div className="embed-responsive embed-responsive-4by3">
         <img className="embed-responsive-item" style={{ objectFit: 'contain' }} src={src} alt={alt} />
       </div>
@@ -14,4 +14,5 @@ export default function ImagePreview({ src, alt }) {
 ImagePreview.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
+  id: PropTypes.string,
 };
--- src/id-verification/panels/SummaryPanel.jsx
+++ src/id-verification/panels/SummaryPanel.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
 import { history } from '@edx/frontend-platform';
-import { Button } from '@edx/paragon';
+import { Input, Button } from '@edx/paragon';
 
 import { useNextPanelSlug } from '../routing-utilities';
 import { submitIdVerfication } from '../data/service';
@@ -12,15 +12,22 @@ import ImagePreview from '../ImagePreview';
 export default function SummaryPanel() {
   const panelSlug = 'summary';
   const nextPanelSlug = useNextPanelSlug(panelSlug);
-  const context = useContext(IdVerificationContext);
+  const {
+    facePhotoFile,
+    idPhotoFile,
+    nameOnAccount,
+    idPhotoName,
+  } = useContext(IdVerificationContext);
+  const nameToBeUsed = idPhotoName || nameOnAccount || '';
+  const courseRunKey = null; // TODO: Implement course run key
 
   function SubmitButton() {
     function handleClick(e) {
       const verificationData = {
-        facePhotoFile: context.facePhotoFile,
-        idPhotoFile: context.idPhotoFile,
-        idPhotoName: context.idPhotoName,
-        courseRunKey: context.courseRunKey,
+        facePhotoFile: facePhotoFile,
+        idPhotoFile: idPhotoFile,
+        idPhotoName: idPhotoName,
+        courseRunKey: courseRunKey,
       };
       const { success, message } = submitIdVerfication(verificationData);
       history.push(nextPanelSlug);
@@ -37,10 +44,34 @@ export default function SummaryPanel() {
       name={panelSlug}
       title="Review Your Photos"
     >
-      <ImagePreview
-        src={context.facePhotoFile}
-        alt='Photo of your face to be submitted.'
-      />
+      <div class="row">
+        <div class="col-6">
+          <label htmlFor="photo-of-face">Your face</label>
+          <ImagePreview
+            id="photo-of-face"
+            src={facePhotoFile}
+            alt='Photo of your face to be submitted.'
+          />
+        </div>
+        <div class="col-6">
+          <label htmlFor="photo-of-id">Your ID</label>
+          <ImagePreview
+            id="photo-of-id"
+            src={idPhotoFile}
+            alt='Photo of your ID to be submitted.'
+          />
+        </div>
+      </div>
+      <div className="form-group">
+          <label htmlFor="name-to-be-used">Name to be used</label>
+          <Input
+            id="name-to-be-used"
+            type="text"
+            readOnly
+            value={nameToBeUsed}
+            onChange={() => {}}
+          />
+      </div>
       <SubmitButton />
     </BasePanel>
   );
