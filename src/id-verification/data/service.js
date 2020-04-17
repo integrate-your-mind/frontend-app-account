bc1375e5ba2edd6e37332db753d6d5f1cfd00fd0
Kyle McCormick <kmccormick@edx.org>
2020-04-17 11:14:35 -0400

    Wire up Submit button

--- src/id-verification/data/service.js
+++ src/id-verification/data/service.js
@@ -7,7 +7,7 @@ import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 /**
  * Submit ID verifiction to LMS.
  *
- * verificationInfo should take the shape of:
+ * verificationData should take the shape of:
  *   - facePhotoFile (String): Base64-encoded image.
  *   - idPhotoFile (String|null): Optional Base64-encoded image
  *   - idPhotoName (String|null): Optional string to change the user's name to.
@@ -16,19 +16,27 @@ import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
  * Returns { success: Boolean, message: String|null }
  */
 // eslint-disable-next-line import/prefer-default-export
-export async function submitIdVerfication(verificationInfo) {
-  const url = `${getConfig().LMS_BASE_URL}/verify_student/submit-photos/`;
-  const postData = qs.stringify({
-    face_photo: verificationInfo.facePhotoFile,
-    photo_id_image: verificationInfo.idPhotoFile,
-    full_name: verificationInfo.idPhotoName,
-    course_id: verificationInfo.courseRunKey,
+export async function submitIdVerfication(verificationData) {
+  const keyMap = {
+    facePhotoFile: 'face_photo',
+    idPhotoFile: 'photo_id_image',
+    idPhotoName: 'full_name',
+    courseRunKey: 'course_id',
+  };
+  const postData = {};
+  // Don't include blank/null/undefined values.
+  // Note that this will also drop the value `false`.
+  Object.keys(keyMap).forEach((key) => {
+    if (verificationData[key]) postData[key] = verificationData[key];
   });
+
+  const url = `${getConfig().LMS_BASE_URL}/verify_student/submit-photos/`;
+  const urlEncodedPostData = qs.stringify(postData);
   const requestConfig = {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   };
   try {
-    await getAuthenticatedHttpClient().post(url, postData, requestConfig);
+    await getAuthenticatedHttpClient().post(url, urlEncodedPostData, requestConfig);
     return { success: true, message: null };
   } catch (e) {
     return { success: false, message: String(e) }; // TODO: is String(e) right?
--- src/id-verification/panels/SummaryPanel.jsx
+++ src/id-verification/panels/SummaryPanel.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { history } from '@edx/frontend-platform';
 import { Button } from '@edx/paragon';
 
 import { useNextPanelSlug } from '../routing-utilities';
+import { submitIdVerfication } from '../data/service';
 import BasePanel from './BasePanel';
+import { IdVerificationContext } from '../IdVerificationContext';
 
 export default function SummaryPanel() {
   const panelSlug = 'summary';
   const nextPanelSlug = useNextPanelSlug(panelSlug);
+  const context = useContext(IdVerificationContext);
 
   function SubmitButton() {
     function handleClick(e) {
+      const verificationData = {
+        facePhotoFile: context.facePhotoFile,
+        idPhotoFile: context.idPhotoFile,
+        idPhotoName: context.idPhotoName,
+        courseRunKey: context.courseRunKey,
+      }
+      const { success, message } = submitIdVerfication(verificationData);
       history.push(nextPanelSlug)
     }
     return (
       <Button className="btn btn-primary" onClick={handleClick}>
-        Submit
+        Confirm
       </Button>
     );
   }
