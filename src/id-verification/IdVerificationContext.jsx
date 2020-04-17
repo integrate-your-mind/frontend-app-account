a1fba076e626286041465212f6f0a0dde091f16d
Kyle McCormick <kmccormick@edx.org>
2020-04-17 17:51:48 -0400

    Clean up IDv-status-gating screens

--- src/id-verification/IdVerificationContext.jsx
+++ src/id-verification/IdVerificationContext.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { AppContext } from '@edx/frontend-platform/react';
+import { getConfig } from '@edx/frontend-platform';
+
 import { hasGetUserMediaSupport } from './getUserMediaShim';
 import { getExistingIdVerification } from './data/service';
+import PageLoading from '../account-settings/PageLoading'
 
 const IdVerificationContext = React.createContext({});
 
@@ -50,23 +53,35 @@ function IdVerificationContextProvider({ children }) {
     },
   };
 
+  // Call verification status endpoint to check whether we can verify.
   useEffect(() => {(async () => {
     const existingIdV = await getExistingIdVerification();
     setExistingIdVerification(existingIdV);
   })()}, []);
 
+  // If we are waiting for verification status endpoint, show spinner.
   if (!existingIdVerification) {
-    return (
-      <div className="page__id-verification container-fluid py-5">
-       <h1>Loading</h1>
-      </div>
-    );
+    return <PageLoading srMessage='Loading verification status' />;
   }
 
   if (!existingIdVerification.canVerify) {
+    const status = existingIdVerification.status;
     return (
-      <div className="page__id-verification container-fluid py-5">
-       <h1>Nah</h1>
+      <div>
+        <h3 aria-level="1" tabIndex="-1">Identity Verification</h3>
+        {status === 'pending' || status == 'approved'
+        ? <p>
+            You have already submitted your verification information.
+            You will see a message on your dashboard when the verification process
+            is complete (usually within 1-2 days).
+          </p>
+        : <p>
+           You cannot verify your identity at this time.
+          </p>
+        }
+        <a className="btn btn-primary" href={`${getConfig().LMS_BASE_URL}/dashboard`}>
+          Return to Your Dashboard
+        </a>
       </div>
     );
   }
