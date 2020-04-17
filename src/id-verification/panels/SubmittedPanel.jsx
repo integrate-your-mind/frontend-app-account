50435746628422292ffbb20faddbd19275babf99
Adam Butterworth <abutterworth@edx.org>
2020-04-17 16:28:48 -0400

    Add content to submitted screen

--- src/id-verification/panels/SubmittedPanel.jsx
+++ src/id-verification/panels/SubmittedPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getConfig } from '@edx/frontend-platform';
 
 import { useNextPanelSlug } from '../routing-utilities';
 import BasePanel from './BasePanel';
@@ -9,9 +10,10 @@ export default function SubmittedPanel() {
   return (
     <BasePanel
       name={panelSlug}
-      title="Submitted panel"
+      title="Identity Verification in Progress"
     >
-      WIP
+      <p>We have received you information and are verifiying your identity. You will see a message on your dashboard when the verification process is complete (usually within 1-2 days). In the meantime, you can still access all available course content.</p>
+      <a className="btn btn-primary" href={`${getConfig().LMS_BASE_URL}/dashboard`}>Return to Your Dashboard</a>
     </BasePanel>
   );
 }
